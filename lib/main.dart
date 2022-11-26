415d12df70cdc1706cc308dc7e90c4be84e3daeb
Bao <21520611@gm.uit.edu.vn>
2022-11-26 14:57:04 +0700

    add: firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-
+//import 'package:firebase_core/firebase_core.dart';
 void main() => runApp(MyApp());
+//Test Firebase
+/*Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
+  runApp(MyApp());
+}*/
 
 class MyApp extends StatelessWidget {
   @override
